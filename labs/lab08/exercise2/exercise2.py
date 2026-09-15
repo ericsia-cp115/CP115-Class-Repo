5a5a0ea63175fdcc202487808549f5a8aec8f999
ericsia-cp115 <ericsialyy@gmail.com>
2026-09-15 11:31:27 +0800

    lab08

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -3,6 +3,29 @@ base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
+gross_salary = base_salary + (overtime_hours*35)
+
+if tax_status == "Single" :
+    if gross_salary>= 5000:
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+
+if tax_status == "Married" :
+    if gross_salary>=6000:
+        tax_rate = 0.20
+    
+    else:
+        tax_rate = 0.15
+
+if tax_status =="Head" :
+    if gross_salary>= 5500:
+        tax_rate = 0.25
+    else: 
+        tax_rate=0.19
+
+
+net_salary = gross_salary - (gross_salary*tax_rate)- (gross_salary*0.11)- (gross_salary*0.005)
 
 
 print(employee_name)
--- labs/lab08/exercise3/exercise3.py
+++ labs/lab08/exercise3/exercise3.py
@@ -2,7 +2,9 @@ day_type = input()
 show_time = int(input())
 customer_type = input()
 
-
+if day_type == "weekend":
+    if show_time > 18:
+        price = 
 
 print(base_price)
 print(final_price)
