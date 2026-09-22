964606ac3b0be805abe7b280c2b46bc7a76debc9
ericsia-cp115 <ericsialyy@gmail.com>
2026-09-22 10:46:48 +0800

    s

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -1,6 +1,19 @@
 num_rounds = int(input())
 
+final_score =0
+rounds_processed =0
 
+for i in range (1, num_rounds + 1):
+    score = int(input("Enter each round's score:"))
+    
+    if score > 100:
+        final_score += score + (score*20/100)
+
+    else:
+        final_score += score
+
+rounds_processed = num_rounds
+    
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -1,7 +1,16 @@
 num_days = int(input())
 danger_threshold = float(input())
 
+danger_days= 0
+average_temp = 0
 
+for i in range (num_days):
+    temperature = float(input("Enter temperature:"))
+
+    if temperature > danger_threshold:
+        above_threshold = danger_days + 1
+
+    average_temp += temperature
 
 print(danger_days)
 print(f"{average_temp:.1f}")
